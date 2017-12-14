c11334199bb8fce5a933d56b1552c0cd3df90d86
Shantanu <shantanu2846@gmail.com>
2017-12-14 14:26:45 +0530

    Add core plugin class

---
+++ includes/class-load-structure-visualiser.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Load structure visualiser
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) )
+	exit();
+
+if ( !class_exists( 'Load_Structure_Visualiser' ) ) {
+
+	/**
+	 * Load structure visualiser
+	 *
+	 * @since 0.0.1
+	 */
+	class Load_Structure_Visualiser {
+		
+	}
+
+	// class
+}// if !class_exists()
+
