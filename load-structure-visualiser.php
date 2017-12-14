1c0fde8bca645988b241ab4e5a5f12b3c1b5702c
Shantanu <shantanu2846@gmail.com>
2017-12-14 14:22:51 +0530

    Add the core plugin file load-structure-visualiser.php

---
+++ load-structure-visualiser.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the
+ * plugin admin area. This file also includes all of the dependencies used by
+ * the plugin, registers the activation and deactivation functions, and defines
+ * a function that starts the plugin.
+ *
+ * @since             0.0.1
+ * @package           load_structure_visualiser
+ *
+ * @wordpress-plugin
+ * 
+ * Plugin Name: Load Structure Visualiser
+ * Description: Helps visualise the load sequence of WordPress hooks, filters, global variables and constants.
+ * Version: 0.0.1
+ * Author: BaapWP
+ * Author URI:  https://github.com/BaapWP
+ * Text Domain: load-structure-visualiser
+ * Domain Path: /languages
+ * License: GPL2
+ */
+
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) exit();
+
+if ( !defined( 'LSV_PATH' ) ) {
+	/**
+	 * Path to the plugin directory.
+	 *
+	 * @since 0.0.1
+	 */
+	define( 'LSV_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+}
+
+if ( !defined( 'LSV_URL' ) ) {
+	/**
+	 * URL to the plugin directory.
+	 *
+	 * @since 0.0.1
+	 */
+	define( 'LSV_URL', trailingslashit( plugin_dir_url(  __FILE__ ) ) );
+}
