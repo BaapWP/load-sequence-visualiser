c6653c626f597454a666212f676f6087accd5eb1
Shantanu <shantanu2846@gmail.com>
2017-12-15 09:13:32 +0530

    Add get raw data method to the core plugin class

--- includes/class-load-structure-visualiser.php
+++ includes/class-load-structure-visualiser.php
@@ -81,6 +81,55 @@ if ( !class_exists( 'Load_Structure_Visualiser' ) ) {
 			add_action( 'shutdown', array( $this, 'print_raw_data' ) );
 		}
 
+		/**
+		 * Get raw data
+		 * 
+		 * Gets the data for included files and defined constants at each hook.
+		 * 
+		 * @return void
+		 * 
+		 * @since 0.0.1
+		 */
+		public function get_raw_data() {
+
+			// Get current filter
+			$current_filter = current_filter();
+
+			// If the $raw_data array already has an entry for that filter, return
+			if ( !empty( $this->raw_data[ $current_filter ] ) ) {
+				return;
+			}
+
+			// Get the files that have been included up to this point
+			$included_files = get_included_files();
+			
+			// Get the constants that have been defined up to this point
+			$defined_constants = get_defined_constants();
+			
+			// Declare an array for filtered included files
+			$filtered_included_files	 = array();
+			
+			// Declare an array for filtered defined constants
+			$filtered_defined_constants	 = array();
+
+			if ( empty( $this->previous_filter ) ) {
+				$this->raw_data[ $current_filter ][ 'includes' ]	 = $included_files;
+				$this->raw_data[ $current_filter ][ 'constants' ] = $defined_constants;
+				$this->all_included_files += $included_files;
+				$this->all_defined_constants += $defined_constants;
+			}
+			else {
+				$filtered_included_files += array_diff( $included_files, $this->all_included_files );
+				$filtered_defined_constants += array_diff_assoc( $defined_constants, $this->all_defined_constants );
+				$this->raw_data[ $current_filter ][ 'includes' ]	 = $filtered_included_files;
+				$this->raw_data[ $current_filter ][ 'constants' ] = $filtered_defined_constants;
+				$this->all_included_files += $filtered_included_files;
+				$this->all_defined_constants += $filtered_defined_constants;
+			}
+
+			$this->previous_filter = $current_filter;
+		}
+
 	}
 
 	// class
