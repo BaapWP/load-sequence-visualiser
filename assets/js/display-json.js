3d2fc4a075d0ac356a9854ddc189a5191d373cca
Shantanu <shantanu2846@gmail.com>
2017-12-19 19:37:26 +0530

    Clear display-json.js

--- assets/js/display-json.js
+++ assets/js/display-json.js
@@ -1,9 +1,3 @@
 jQuery( 'document' ).ready( function ( $ ) {
-    
-    var output = $( '<div class="lsv-output" />' );
-
-    output.html(JSON.parse(JSON.stringify(load_sequence)));
-
-    $("content.site-content").append(output);
 
 } );
