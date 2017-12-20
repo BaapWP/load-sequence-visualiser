f98cfb443c238a9aacdcfbea9bc52562bcbb5eed
Shantanu <shantanu2846@gmail.com>
2017-12-20 15:32:42 +0530

    Print the output on the browser console to test

--- assets/js/display-json.js
+++ assets/js/display-json.js
@@ -1,3 +1,19 @@
 jQuery( 'document' ).ready( function ( $ ) {
+    
+    var js_object = JSON.parse( JSON.stringify( load_sequence ) );
+        
+    $.each( js_object, lsv_display);
+    
+    function lsv_display(key, value) {
+	
+	if ( value !== null && typeof (vaule) !== "object" ) {
+	    console.log(value);
+	}
+	
+	if( value !== null && typeof ( value) === "object" ) {
+	    $.each( value, lsv_display);
+	}
+	
+    }
 
 } );
