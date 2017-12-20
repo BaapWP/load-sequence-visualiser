b3d7ed81f37aed1c550e34e31ac97d5c6fb1e988
Shantanu <shantanu2846@gmail.com>
2017-12-20 17:40:47 +0530

    Update the jQuery to output the array

--- assets/js/display-json.js
+++ assets/js/display-json.js
@@ -1,19 +1,21 @@
 jQuery( 'document' ).ready( function ( $ ) {
-    
-    var js_object = JSON.parse( JSON.stringify( load_sequence ) );
-        
-    $.each( js_object, lsv_display);
-    
-    function lsv_display(key, value) {
-	
-	if ( value !== null && typeof (vaule) !== "object" ) {
-	    console.log(value);
-	}
-	
-	if( value !== null && typeof ( value) === "object" ) {
-	    $.each( value, lsv_display);
-	}
-	
-    }
 
+    if ( load_sequence !== "undefined" ) {
+	var output_div = $( '<div class="lsv-output" style="background-color:red" />' );
+	
+	
+	var js_object = JSON.parse( JSON.stringify( load_sequence ) );
+
+	$.each( js_object, lsv_display );
+
+	function lsv_display( key, value ) {
+
+	    output_div.append( value);
+	    output_div.append( key );
+	    console.log( value );
+	    console.log( key );
+	}
+	$( "footer.site-footer" ).prepend( output_div );
+    }   
+    
 } );
