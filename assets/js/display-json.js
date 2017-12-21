e8d743fd395ca65c6ecc6f416d7e96a8a4e22f7d
Shantanu <shantanu2846@gmail.com>
2017-12-21 09:27:34 +0530

    Print a css class corresponding to the type of the output

--- assets/js/display-json.js
+++ assets/js/display-json.js
@@ -2,20 +2,29 @@ jQuery( 'document' ).ready( function ( $ ) {
 
     if ( load_sequence !== "undefined" ) {
 	var output_div = $( '<div class="lsv-output" style="background-color:red" />' );
-	
-	
+
+	output_div.append( "<ul>OUTPUT" );
 	var js_object = JSON.parse( JSON.stringify( load_sequence ) );
 
 	$.each( js_object, lsv_display );
 
 	function lsv_display( key, value ) {
 
-	    output_div.append( value);
-	    output_div.append( key );
-	    console.log( value );
-	    console.log( key );
+	    class_name = '';
+	    if ( 'file' === value ) {
+		class_name = "lsv-file";
+	    } else if ( 'constant' === value ) {
+		class_name = "lsv-constant";
+	    } else if ( 'global' === value ) {
+		class_name = "lsv-global";
+	    } else {
+		class_name = "lsv-filter";
+	    }
+	    output_div.append( "<li class=" + class_name + ">" + value + ":" + key + "</li>" );
+	    console.log( value + ":" + key );
 	}
+	output_div.append( "OUTPUT</ul>" );
 	$( "footer.site-footer" ).prepend( output_div );
     }
-    
+
 } );
